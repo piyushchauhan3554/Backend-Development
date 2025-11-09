867392df705ff3c2cb4e6b8f973cf6bd04a63629
Piyush Chauhan <piyushchauhan3554@gmail.com>
2025-11-09 23:56:32 +0530

    add

--- Rest-APIs/server.js
+++ Rest-APIs/server.js
@@ -7,15 +7,22 @@ app.use(express.urlencoded({extended:true}))
 
 // view engine set to ejs
 app.set("view engine","ejs")
-
-
 app.set("views",path.join(__dirname,"views"))
 
 app.use(express.static(path.join(__dirname,"public"))) // for static files : like css file 
 
+let posts=[{
+  username:"piyush chauhan",
+  content:"my favourite ipl team is rcb"
+},{
+  username:"abhinav garg",
+  content:"hardwork is a key to success"},{
+  username:"vansh sharma",
+  content:"go to gym everyday"
+}]
 
-app.get("/",(req,res)=>{
-  res.send("server is live")
+app.get("/posts",(req,res)=>{
+  res.render("index.ejs",{posts});
 })
 const PORT=3000
 app.listen(PORT,()=>{
