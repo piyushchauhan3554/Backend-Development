9499e70a27d465d2c77e7884527e1cdf5cd3cc4e
Piyush Chauhan <piyushchauhan3554@gmail.com>
2025-11-09 20:04:36 +0530

    Update server.js

--- Rest-APIs/server.js
+++ Rest-APIs/server.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const express=require("express")
+const path=require("path")
+const app=express()
+
+// client to server data: parse by express server
+app.use(express.urlencoded({extended:true}))
+
+// view engine set to ejs
+app.set("view engine","ejs")
+
+
+app.set("views",path.join(__dirname,"views"))
+
+app.use(express.static(path.join(__dirname,"public"))) // for static files : like css file 
+
+const PORT=3000
+app.listen(PORT,()=>{
+  console.log(`server is listening at localhost:${PORT}`);
+})
