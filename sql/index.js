07dfc50a8df5c9cafec76aff8a4be0a519deae51
Piyush Chauhan <piyushchauhan3554@gmail.com>
2025-12-14 20:32:10 +0530

    add

--- sql/index.js
+++ sql/index.js
@@ -1,5 +1,11 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -8,9 +14,6 @@ const connection = mysql.createConnection({
   database: "delta_app",
 });
 
-// insert data into table using placeholders, (dynamic data)
-let q = "insert into users(id,username,email,password) values ?";
-let data = [];
 const createUser = () => {
   return [
     faker.string.uuid(),
@@ -20,16 +23,57 @@ const createUser = () => {
   ];
 };
 
-for(let i=1;i<=50;i++) data.push(createUser());
-try {
-  connection.query(q, [data], (err, result) => {
-    if (err) throw err;
-    // console.log("number of tables : " + result.length);
-    // for (let i = 0; i < result.length; i++) console.log(result[i]);
-  });
-} catch (error) {
-  console.log(error);
-}
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server is listening at PORT:${port}`);
+});
 
+// routes
 
-connection.end();
+// get no. of users (home route)
+app.get("/", (req, res) => {
+  let q = "select count(*) from users";
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let userCnt = result[0]["count(*)"];
+      // console.log(`no. of users are` + userCnt);
+      res.render("Home.ejs", { userCnt });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// display all users data
+// show route
+
+app.get("/users", (req, res) => {
+  try {
+    let q = "select * from users";
+    connection.query(q, (err, users) => {
+      if (err) throw err;
+      // console.log(users);
+      res.render("show.ejs", { users });
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.send("DB Error");
+  }
+});
+
+// insert data into table using placeholders, (dynamic data)
+// let q = "insert into users(id,username,email,password) values ?";
+// let data = [];
+
+// for(let i=1;i<=100;i++) data.push(createUser());
+// try {
+//   connection.query(q, [data], (err, result) => {
+//     if (err) throw err;
+//   });
+// } catch (error) {
+//   console.log(error);
+// }
+
+// connection.end();
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,8 @@
-create table users(
-  id varchar(50) primary key,
-  username varchar(50) unique,
-  email varchar(50) not null unique,
-  password varchar(50) not null
-)
+-- create table users(
+--   id varchar(50) primary key,
+--   username varchar(50) unique,
+--   email varchar(50) not null unique,
+--   password varchar(50) not null
+-- )
+
+insert into users(id,username,email,password) values("123","@piyush","piyush@gmail.com","1323231")
