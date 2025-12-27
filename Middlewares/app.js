173e06658b8ea412132047002af672b3faa61ce6
Piyush Chauhan <piyushchauhan3554@gmail.com>
2025-12-27 23:12:36 +0530

    add

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -4,15 +4,30 @@ const app=express()
 
 // middleware
 
-app.use((req,res,next)=>{
-  console.log('i am the 1st middleware');
-  return next()
-})
+// app.use((req,res,next)=>{
+//   console.log('i am the 1st middleware');
+//   return next()
+// })
 
 
-app.use((req,res,next)=>{
-  console.log('i am the 2nd middleware');
-  next()
+// app.use((req,res,next)=>{
+//   console.log('i am the 2nd middleware');
+//   next()
+// })
+
+// utility middleware : perform some task
+// custom logger : log imp info on console // pre-built : morgan (3rd party middleware)
+
+// app.use((req,res,next)=>{
+//   req.time=new Date();
+//   console.log(`method: ${req.method}   path: ${req.path}   host: ${req.host}  time: ${req.time.toString().split(" ")[4]} date: ${req.time.toString().split(" ").slice(0,4).join(" ")}`);
+//   next();
+// })
+
+// middleware
+app.use("/random",(req,res,next)=>{
+  console.log('i am for /random route');
+  next();
 })
 
 // routes/ api endpoints
@@ -22,6 +37,11 @@ app.get("/",(req,res)=>{
 
 app.get("/random",(req,res)=>{
   res.send("i am random route");
+})
+
+// error middleware
+app.use((req,res)=>{
+  res.status(404).send("Page Not Found")
 })
 
 app.listen(3000,()=>{
